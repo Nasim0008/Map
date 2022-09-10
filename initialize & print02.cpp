316d606440974570a8affb81e643670b1866037e
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-09-10 23:52:04 +0600

    initialize and print the map

---
+++ initialize & print02.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    map<string,string> m;
+    m["nasim"]= "Male";
+    m["shohan"]="Male";
+    m["Omaer"]="male";
+    m["shakib"]="male";
+    m["lamia"]="female";
+
+    cout<< m["nasim"]<<endl<< m["shohan"]<<endl<< m["Omaer"]<<endl<< m["shakib"]<<endl<<m["lamia"]<<endl;
+
+}
+
