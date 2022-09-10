e1404013bed6c551a075d9cee49dfd0fb682b3b2
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-09-10 23:37:31 +0600

    initialize and print the mapping

---
+++ initialize & print.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    map<string,int> m;
+    m["nasim"]=10;
+    m["shohan"]=20;
+    m["Omaer"]=30;
+    m["shakib"]=40;
+
+    cout<< m["nasim"]<<endl<< m["shohan"]<<endl<< m["Omaer"]<<endl<< m["shakib"];
+
+}
