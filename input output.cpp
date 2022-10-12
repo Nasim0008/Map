410d83cf61aa4da1e209c83ed9737e5fc3727b48
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-10-12 15:43:29 +0600

    map input and output

---
+++ input output.cpp
@@ -0,0 +1,22 @@
+
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    map<int,int>m;
+
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        int a,b;
+        cin>>a>>b;
+        m.insert({a,b});
+
+    }
+    for(auto u:m)
+    {
+        cout<<u.first<< " "<<u.second<<endl;
+    }
+
+}
