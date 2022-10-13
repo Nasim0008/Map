4ae80dc04f55a46e442f98f4b35656d3703564ed
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2022-10-13 11:57:06 +0600

    input and print unordered_map

---
+++ unordered map print.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+   unordered_map<char,int>m;
+    int n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        char a;int b;
+        cin>>a>>b;
+        m.insert({a,b});
+    }
+   for(auto u:m)
+   {
+       cout<<u.first<< " "<<u.second<<endl;
+   }
+}
+
