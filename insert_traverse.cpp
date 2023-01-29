30b6f63f5d9333347464bcfecc9132985cbc7ca1
Md Nasim Hossen <102505098+Nasim0008@users.noreply.github.com>
2023-01-29 11:02:08 +0600

    insert and traverse the map

---
+++ insert_traverse.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    map<int,string>m;
+    m[4]="nasim";
+    m[2]="tesla";
+    m.insert({3,"micro"});
+    m.insert({5,"ayesha"});
+    map<int,string>:: iterator it;
+    for(auto it=m.begin();it!=m.end();it++)
+    {
+        cout<<(*it).first<< " "<<(*it).second<<endl;
+    }
+    
+}
